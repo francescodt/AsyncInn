2c2a45b1d98c6659bb57150649590f132457c82d
Francesco <francescodtroiano@gmail.com>
2020-05-20 21:13:15 -0500

    actually the last bit of set-up

---
+++ AsyncInn/AsyncInn/Data/DBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsyncInn.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+
+        public AsyncInnDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
