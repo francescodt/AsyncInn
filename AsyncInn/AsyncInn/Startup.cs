c294cc562a468e479e6fb97edfd2d646d9556206
Francesco <francescodtroiano@gmail.com>
2020-05-26 22:07:39 -0500

    meant to be on another branch. am switching, but added the two folders I need

--- AsyncInn/AsyncInn/Startup.cs
+++ AsyncInn/AsyncInn/Startup.cs
@@ -35,6 +35,8 @@ namespace AsyncInn
                 ("DefaultConnection");
                 options.UseSqlServer(connectionString);
             });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
